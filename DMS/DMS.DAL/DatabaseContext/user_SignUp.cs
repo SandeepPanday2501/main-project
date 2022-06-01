9fff2a42e4fcbf6070ccddb70a6b33b350d56f31
saugat330 <ssaugat349@gmail.com>
2022-06-01 15:22:02 +0545

    userlogin

--- DMS/DMS.DAL/DatabaseContext/user_SignUp.cs
+++ DMS/DMS.DAL/DatabaseContext/user_SignUp.cs
@@ -11,12 +11,19 @@ namespace DMS.DAL.DatabaseContext
 {
     using System;
     using System.Collections.Generic;
-    
-    public partial class tbl_employee
+    using System.ComponentModel.DataAnnotations;
+
+    public partial class user_SignUp
     {
+        
         public int id { get; set; }
-        public string name { get; set; }
-        public string address { get; set; }
-        public string contact { get; set; }
+        [Required(ErrorMessage = "This field is required!!")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "This field is required!!")]
+        public string password { get; set; }
+        public string loginerrorMessage { get; set; }       
+
+        [Compare("password")]
+        public string confirmpassword { get; set; }
     }
 }
